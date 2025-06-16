feb7ab75b8550115836f71ab06ccb22789405e8a
Si Fad <sifayed00@gmail.com>
2025-06-17 00:40:37 +0600

    i added axios request and response interceptors and change the ui of home categories sections card

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -6,12 +6,30 @@ const axiosInstance = axios.create({
     baseURL: 'http://localhost:3000'
 });
 const useAxiosSecure = () => {
-    const { user } = useAuth();
+    const { user, signOutUser } = useAuth();
+
+    // request interceptor
     axiosInstance.interceptors.request.use(config => {
         if (user?.accessToken) {
             config.headers.Authorization = `Bearer ${user.accessToken}`;
         };
         return config;
+    });
+
+    // response interceptor 
+    axiosInstance.interceptors.response.use(response => {
+        return response;
+    }, error => {
+        if (error.status === 401) {
+            signOutUser()
+                .then(() => {
+                    console.log('Sign out user for 401 status code!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+        return Promise.reject(error);
     });
 
     return axiosInstance;
--- src/pages/Home/Categories.jsx
+++ src/pages/Home/Categories.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { FaCertificate } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
 
 const books = [
     {
-        "bookImg": "https://i.postimg.cc/8CDr8DQV/history-1.jpg",
+        "bookImg": "https://i.postimg.cc/VLn9Gz5Z/images.jpg",
         "category": "History"
     },
     {
-        "bookImg": "https://i.postimg.cc/1R0gyhRT/thriller.jpg",
+        "bookImg": "https://i.postimg.cc/fRr566LX/image-2.jpg",
         "category": "Thriller"
     },
     {
-        "bookImg": "https://i.postimg.cc/YqN1BdXc/novel.jpg",
+        "bookImg": "https://i.postimg.cc/qMVyCWQs/image-3.jpg",
         "category": "Novel"
     },
     {
-        "bookImg": "https://i.postimg.cc/GhfKs3RN/drama-4.jpg",
+        "bookImg": "https://i.postimg.cc/Y2QdPnYD/image-4.jpg",
         "category": "Drama"
     }
 ]
 
 const Categories = () => {
     return (
-        <section className='bg-[#d0e1e7] dark:bg-[var(--color-bg)] xl:px-40 md:px-4 sm:px-10 sm:py-20 py-10 my-10'>
-            <div className='flex flex-col sm:flex-row items-center justify-between sm:mb-6 mb-4 xl:px-9 lg:px-6 md:px-12 gap-1'>
+        <section className='bg-[#d0e1e7] dark:bg-[var(--color-bg)] xl:px-40 md:px-6 sm:py-26 px-4 py-16 my-10'>
+            <div className='flex flex-col sm:flex-row items-center justify-between sm:mb-6 mb-4 xl:px-9 lg:px-6 md:px-12 px-6 gap-2'>
                 <h1 className='text-2xl md:text-3xl font-semibold text-[var(--color-dark-secondary)] dark:text-[var(--color-light-secondary)]'>Book Categories</h1>
                 <Link to='/all-books'>
                     <button className='relative overflow-hidden group text-xs font-semibold sm:px-6 sm:py-[10px] px-4 py-2 border border-[var(--color-dark-secondary)] dark:border-[var(--color-light-secondary)] text-[var(--color-dark-secondary)] dark:text-[var(--color-light-secondary)] hover:text-white  tarnsition-all duration-500 rounded-full'>
@@ -38,16 +38,15 @@ const Categories = () => {
             <div className="flex flex-wrap mx-auto justify-center items-center gap-4">
                 {books.map((book, index) => (
                     <Link to={`/category-books/${book.category}`} key={index} className="rounded-xl transition-all duration-300 hover:-translate-y-2">
-                        <div className='relative  bg-[#f5f5f5] dark:bg-[#374151] p-6 flex flex-col justify-center items-center rounded-xl'>
-                            <div className='absolute top-20 z-30 text-[var(--color-dark-secondary)] text-lg font-semibold animate-[spin_2s_linear_infinite]'>
-                                <FaCertificate size={90} />
-                                <span className='text-white absolute top-8 left-4'> {book.category}</span>
-                            </div>
+                        <div className='relative flex flex-col bg-[#f5f5f5] dark:bg-[#374151] p-[10px] justify-center items-center rounded-full'>
                             <img
                                 src={book.bookImg}
                                 alt={book.bookTitle}
-                                className="w-[180px] h-[200px] object-cover rounded"
+                                className="lg:w-[200px] lg:h-[300px] md:w-[180px] md:h-[260px] sm:w-[140px] sm:h-[200px] w-[200px] h-[300px] border-10 border-orange-300 object-cover rounded-full"
                             />
+                            <div className='absolute  text-[var(--color-dark-secondary)] bg-[#f5f5f5] w-18 h-18 flex gap-[2px] justify-center mx-auto items-center font-bold rounded-full mt-1'>
+                                <span className=' text-[10px]'><FaBook /></span><span className='text-xs'> {book.category}</span>
+                            </div>
                         </div>
                     </Link>
                 ))}
